243a59d67a9a052972bd90daece957d36e811fb6
Gleidson <gleidsonsou@gmail.com>
2017-02-10 23:55:23 -0300

    Carousel Imagens Adicionadas

--- resources/views/mlogin.blade.php
+++ resources/views/mlogin.blade.php
@@ -57,10 +57,10 @@
 
       <form class="form-signin" action="\login" method="POST"  onSubmit="return validatel(this);" name="form_mlogin">
         <h2 class="form-signin-heading">Please sign in</h2>
-        	<label for="inputEmail" class="sr-only">Email address</label>
+        	<label for="inputEmail" class="sr-only">Email</label>
 					Email: <input type="text" value="noadmin@ifpb.edu.br" name="email" id="inputEmail" class="form-control" required autofocus> <!--^[a-z0-9._%+-]+@ifpb\.edu\.br$-->
-					<label for="inputPassword" class="sr-only">Password</label>
-      		<input type="password" id="inputPassword" name="password" class="form-control" placeholder="Password" required>
+					<label for="inputPassword" class="sr-only">Senha</label>
+      		Senha:<input type="password" id="inputPassword" name="password" class="form-control" placeholder="Password" required>
 			<button class="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
       </form>
 
--- resources/views/tutorial.blade.php
+++ resources/views/tutorial.blade.php
@@ -12,6 +12,7 @@
 $('.carousel').carousel({
   interval: false
 })
+
 </script>
 <div id="myCarousel" class="carousel slide" data-ride="carousel" data-interval="false">
 <!-- Indicators -->
@@ -19,35 +20,113 @@ $('.carousel').carousel({
     <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
     <li data-target="#myCarousel" data-slide-to="1"></li>
     <li data-target="#myCarousel" data-slide-to="2"></li>
-    
+    <li data-target="#myCarousel" data-slide-to="3"></li>
+    <li data-target="#myCarousel" data-slide-to="4"></li>
+    <li data-target="#myCarousel" data-slide-to="5"></li>
+    <li data-target="#myCarousel" data-slide-to="6"></li>
+    <li data-target="#myCarousel" data-slide-to="7"></li>
+    <li data-target="#myCarousel" data-slide-to="8"></li>
+    <li data-target="#myCarousel" data-slide-to="9"></li>
+    <li data-target="#myCarousel" data-slide-to="10"></li>
+    <li data-target="#myCarousel" data-slide-to="11"></li>
+    <li data-target="#myCarousel" data-slide-to="12"></li>
 </ol>
 <!-- Wrapper for slides -->
 <div class="carousel-inner" role="listbox">
     <div class="item active">
-        <img src="http://kagoceramic.com/kagopic/b/118.jpg" alt="azul" width="460" height="345">
+        <img src="./image/tutorial/inicialnlogado.png" alt="Tela Inicial" width="460" height="345">
         <div class="carousel-caption">
-            <h3>Azul</h3>
+            <h3 style= "color: Black;">Página Inicial</h3>
             <!--<p>The atmosphere in Chania has a touch of Florence and Venice.</p>-->
         </div>
     </div>
 
     <div class="item">
-        <img src="http://www.nationaltiles.com.sg/upload/product/pic-0-526-2.jpg" alt="branco" width="460" height="345">
-         <div class="carousel-caption">
-            <h3>Branco</h3>
+        <img src="./image/tutorial/mlogin.png" alt="Tela Login" width="460" height="345">
+        <div class="carousel-caption">
+            <h3 style= "color: Black;">Login</h3>
             <!--<p>The atmosphere in Chania has a touch of Florence and Venice.</p>-->
         </div>
     </div>
 
     <div class="item">
-    <img src="http://www.clayhausceramics.com/images/glossy/clover.jpg" alt="verde" width="460" height="345">
-     <div class="carousel-caption">
-            <h3>Verde</h3>
+        <img src="./image/tutorial/iniciallogado.png" alt="Tela inicio logado" width="460" height="345">
+        <div class="carousel-caption">
+            <h3 style= "color: Black;">Página Inicial Logado</h3>
             <!--<p>The atmosphere in Chania has a touch of Florence and Venice.</p>-->
         </div>
     </div>
 
-    
+    <div class="item">
+        <img src="./image/tutorial/formeventosclear.png" alt="Formulario eventos clear" width="460" height="345">
+        <div class="carousel-caption">
+            <h3 style= "color: Black;">Formulario eventos vazio</h3>
+            <!--<p>The atmosphere in Chania has a touch of Florence and Venice.</p>-->
+        </div>
+    </div>
+    <div class="item">
+        <img src="./image/tutorial/formeventoscomplete.png" alt="Formulario eventos complete" width="460" height="345">
+        <div class="carousel-caption">
+            <h3 style= "color: Black;">Formulario eventos completo</h3>
+            <!--<p>The atmosphere in Chania has a touch of Florence and Venice.</p>-->
+        </div>
+    </div>
+    <div class="item">
+        <img src="./image/tutorial/tela_suc_cad_evento.png" alt="Tela Sucesso Eventos" width="460" height="345">
+        <div class="carousel-caption">
+            <h3 style= "color: Black;">Tela Sucesso Eventos</h3>
+            <!--<p>The atmosphere in Chania has a touch of Florence and Venice.</p>-->
+        </div>
+    </div>
+    <div class="item">
+        <img src="./image/tutorial/tela_erro_cad_evento.png" alt="Tela Erro Eventos" width="460" height="345">
+        <div class="carousel-caption">
+            <h3 style= "color: Black;">Tela Erro Eventos</h3>
+            <!--<p>The atmosphere in Chania has a touch of Florence and Venice.</p>-->
+        </div>
+    </div>
+    <div class="item">
+        <img src="./image/tutorial/eventou1.png" alt="Eventos" width="460" height="345">
+        <div class="carousel-caption">
+            <h3 style= "color: Black;">Eventos</h3>
+            <!--<p>The atmosphere in Chania has a touch of Florence and Venice.</p>-->
+        </div>
+    </div>
+    <div class="item">
+        <img src="./image/tutorial/eventou2.png" alt="Eventos Edit" width="460" height="345">
+        <div class="carousel-caption">
+            <h3 style= "color: Black;">Eventos Editar</h3>
+            <!--<p>The atmosphere in Chania has a touch of Florence and Venice.</p>-->
+        </div>
+    </div>
+    <div class="item">
+        <img src="./image/tutorial/eventouAD.png" alt="Eventos Edit Delete" width="460" height="345">
+        <div class="carousel-caption">
+            <h3 style= "color: Black;">Eventos Atualizar/Deletar</h3>
+            <!--<p>The atmosphere in Chania has a touch of Florence and Venice.</p>-->
+        </div>
+    </div>
+    <div class="item">
+        <img src="./image/tutorial/mc1.png" alt="Conta Usuário" width="460" height="345">
+        <div class="carousel-caption">
+            <h3 style= "color: Black;">Conta do Usuário</h3>
+            <!--<p>The atmosphere in Chania has a touch of Florence and Venice.</p>-->
+        </div>
+    </div>
+    <div class="item">
+        <img src="./image/tutorial/mc2.png" alt="Conta Usuário Edit" width="460" height="345">
+        <div class="carousel-caption">
+            <h3 style= "color: Black;">Conta do Usuário Editar</h3>
+            <!--<p>The atmosphere in Chania has a touch of Florence and Venice.</p>-->
+        </div>
+    </div>
+    <div class="item">
+        <img src="./image/tutorial/mc3.png" alt="Conta Usuário Atualizar" width="460" height="345">
+        <div class="carousel-caption">
+            <h3 style= "color: Black;">Conta do Usuário Atualizar</h3>
+            <!--<p>The atmosphere in Chania has a touch of Florence and Venice.</p>-->
+        </div>
+    </div>
 </div>
 
 <!-- Left and right controls -->
